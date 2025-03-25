187ad75b633d282661c0f3bb606656f0d5c8e6ed
Savchuk Volodymyr <savchuk.v03@gmail.com>
2025-03-25 19:30:09 +0200

    Implement /read-from-image route

--- AI/main.py
+++ AI/main.py
@@ -1,22 +1,31 @@
 import easyocr
 import base64
-from io import BytesIO
 from PIL import Image
+from io import BytesIO
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 
-def read_text_from_base64(base64_string):
-    image_data = base64.b64decode(base64_string)
-    image = Image.open(BytesIO(image_data))
-    
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    
-    reader = easyocr.Reader(['en', "uk", "ru"])
-    
-    result = reader.readtext(buffered.getvalue(), detail=0)
-    
-    return " ".join(result)
+app = FastAPI()
+reader = easyocr.Reader(['en'])
 
-base64_string = ""
-text = read_text_from_base64(base64_string)
+class ImageRequest(BaseModel):
+    image: str
 
-print("Розпізнаний текст:", text)
+def decode_text_from_base64(base64_string: str) -> str:
+    try:
+        image_data = base64.b64decode(base64_string)
+        image = Image.open(BytesIO(image_data))
+        buffered = BytesIO()
+        image.save(buffered, format="PNG")
+        result = reader.readtext(buffered.getvalue(), detail=0)
+        return " ".join(result)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error processing image: {str(e)}")
+
+@app.post("/read-from-image")
+def read_from_image(request: ImageRequest):
+    base_64_parts = request.image.split("base64,")
+    if len(base_64_parts) != 2:
+        raise HTTPException(status_code=400, detail="Invalid image data")
+    text = decode_text_from_base64(base_64_parts[1])
+    return {"decoded_text": text}
