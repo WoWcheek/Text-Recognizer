b37ec6c358baf806a6b448682506338768474878
Savchuk Volodymyr <savchuk.v03@gmail.com>
2025-03-31 20:32:36 +0300

    Implement text recognition

--- Bot/main.py
+++ Bot/main.py
@@ -1,8 +1,9 @@
 import json
+import base64
 import logging
 import asyncio
 import aiohttp
-from aiogram.types import Message
+from aiogram.types import Message, PhotoSize
 from aiogram import Bot, Dispatcher, Router
 
 with open("appsettings.json", "r") as file:
@@ -14,6 +15,27 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 router = Router()
 dp.include_router(router)
+
+@router.message(lambda message: message.photo)
+async def process_image(message: Message):
+    photo: PhotoSize = message.photo[-1]
+    file = await bot.get_file(photo.file_id)
+    file_path = file.file_path
+    file_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(file_url) as resp:
+            image_data = await resp.read()
+            image_b64 = "data:image/png;base64," + base64.b64encode(image_data).decode("utf-8")
+
+            logging.info(f"Encoded image: {image_b64[:50]}...")
+            
+            payload = {"image": image_b64}
+            async with session.post(f"{BACKEND_URL}/image/telegram-read-from-image?telegram_id={message.from_user.id}", json=payload) as response:
+                response_json = await response.json()
+                logging.info(f"Response from image processing: {response_json}")
+                decoded_text = response_json.get("decoded_text", "Не вдалося розпізнати текст.")
+                await message.answer(f"Розпізнаний текст: {decoded_text}")
 
 @router.message(lambda message: message.text == "/start")
 async def start_command(message: Message):
@@ -38,6 +60,22 @@ async def get_email(message: Message):
                 await message.answer("Ви вже підключили свій акаунт до Telegram.")
             else:
                 await message.answer("Виникла помилка при обробці запиту.")
+
+@router.message(lambda message: message.text == "/me")
+async def get_user_info(message: Message):
+    user_data = {"telegram_id": str(message.from_user.id)}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{BACKEND_URL}/telegram/me", json=user_data) as response:
+            response_json = await response.json()
+            logging.info(f"Response from /telegram/me: {response_json}")
+            await message.answer(f"Ваше ім'я: {response_json.get('user').get('name')}\n"
+                                 f"Ваша пошта: {response_json.get('user').get('email')}\n"
+                                 f"Ваша підписка: {response_json.get('user').get('subscription').get('type')}\n"
+                                 f"Ваш поточний ліміт: {response_json.get('user').get('limits').get('count')} запитів\n")
+
+@router.message(lambda message: message.text == "/image")
+async def request_image(message: Message):
+    await message.answer("Будь ласка, надішліть зображення з текстом для розпізнавання.")
 
 async def main():
     logging.basicConfig(level=logging.INFO)
